09f8247e0b065a8dd2b7c8b6515feab7b4fcdf97
oldliutou <1548648078@qq.com>
2022-10-22 16:11:44 +0800

    update

--- python编程：入门到实践/第八章/mkaing_pizzas.py
+++ python编程：入门到实践/第八章/mkaing_pizzas.py
@@ -2,3 +2,4 @@ from .pizza import *
 # pizza.make_pizza(16, 'pepperoni')
 # pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
